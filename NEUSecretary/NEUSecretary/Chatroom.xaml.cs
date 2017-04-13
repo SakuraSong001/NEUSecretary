ca6d2db846a6e8812249ecaa490ee327ccf78b7a
Leo Liu <1010820319@qq.com>
2017-04-13 23:31:47 +0800

    add chatroom and turing robot

---
+++ NEUSecretary/NEUSecretary/Chatroom.xaml.cs
@@ -0,0 +1,334 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Networking;
+using Windows.Networking.Connectivity;
+using Windows.Networking.Sockets;
+using Windows.Storage.Streams;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace NEUSecretary
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class Chatroom : Page
+    {
+
+        StreamSocket socket;
+        bool first = true;
+        bool fresh = false;
+        public string msg;
+        public Chatroom()
+        {
+            this.InitializeComponent();
+        }
+
+        StreamSocketListener listener = null;
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (listener != null)
+            {
+                listener.ConnectionReceived -= OnConnReceived;
+                listener.Dispose();
+                listener = null;
+            }
+
+            listener = new StreamSocketListener();
+            listener.ConnectionReceived += OnConnReceived;
+            await listener.BindServiceNameAsync("");
+
+            runPort.Text = listener.Information.LocalPort;
+
+            //=========================================================
+            var hosts = NetworkInformation.GetHostNames();
+            foreach (HostName h in hosts)
+            {
+                if (h.IPInformation != null && h.Type == HostNameType.Ipv4)
+                {
+                    runIp.Text = h.DisplayName;
+                    break;
+                }
+            }
+        }
+
+        private async void OnConnReceived(StreamSocketListener sender, StreamSocketListenerConnectionReceivedEventArgs args)
+        {
+
+            if (first)
+            {
+                // 获取用于通信的socket
+                //StreamSocket socket = args.Socket;
+                // 向客户端发送字符串：
+                //        你好，我是你外公，我叫服务器。
+                DataWriter writer = new DataWriter(args.Socket.OutputStream);
+
+                string content = "Leeeeo joined in.";
+                //await new MessageDialog(content,"content").ShowAsync();
+                writer.UnicodeEncoding = UnicodeEncoding.Utf8; //注意
+                                                               // 计算长度
+                uint len = writer.MeasureString(content);
+                // 写入长度
+                writer.WriteUInt32(len);
+                // 再写内容
+                writer.WriteString(content);
+                // 提交数据
+                await writer.StoreAsync();
+                first = false;
+                // 这个socket不要了，扔掉
+                //socket.Dispose();
+            }
+            DataReader reader = new DataReader(args.Socket.InputStream);
+            try
+            {
+                while (true)
+                {
+                    uint sizeFieldCount = await reader.LoadAsync(sizeof(uint));
+                    if (sizeFieldCount != sizeof(uint))
+                    {
+                        return;
+                    }
+                    uint stringLength = reader.ReadUInt32();
+                    uint actualStringLength = await reader.LoadAsync(stringLength);
+                    if (stringLength != actualStringLength)
+                    {
+                        return;
+                    }
+                    msg = reader.ReadString(actualStringLength);
+                    // 获取用于通信的socket
+                    //StreamSocket socket = args.Socket;
+                    // 向客户端发送字符串：
+                    //        你好，我是你外公，我叫服务器。
+                    if (msg != null)
+                    {
+                        DataWriter writer = new DataWriter(args.Socket.OutputStream);
+
+                        string content = "Leeeeo:" + msg;
+                        //await new MessageDialog(content,"content").ShowAsync();
+                        writer.UnicodeEncoding = UnicodeEncoding.Utf8; //注意
+                                                                       // 计算长度
+                        uint len = writer.MeasureString(content);
+                        // 写入长度
+                        writer.WriteUInt32(len);
+                        // 再写内容
+                        writer.WriteString(content);
+                        // 提交数据
+                        await writer.StoreAsync();
+                        
+                    }
+
+
+                    /*
+                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        TextBlock tb = new TextBlock { Text = msg, FontSize = 20 };
+                        lbMsg.Children.Add(tb);
+                    });
+                    */
+                }
+            }
+            catch (Exception exception)
+            {
+                if (SocketError.GetStatus(exception.HResult) == SocketErrorStatus.Unknown)
+                {
+                    throw;
+                }
+            }
+
+
+
+        }
+
+        private async void OnConnectToServer(object sender, RoutedEventArgs e)
+        {
+            if (txtIp.Text.Length == 0 || txtPort.Text.Length == 0)
+                return;
+            runRecMsg.Text = "";
+
+            Button b = sender as Button;
+            b.IsEnabled = false;
+
+            socket = new StreamSocket();
+            //StreamSocket socket = new StreamSocket();
+            try
+            {
+                HostName svname = new HostName(txtIp.Text);
+
+                // 连接
+                await socket.ConnectAsync(svname, txtPort.Text);
+
+                // 接收数据
+                DataReader reader = new DataReader(socket.InputStream);
+                reader.UnicodeEncoding = UnicodeEncoding.Utf8; //注意
+                // 长度
+                await reader.LoadAsync(sizeof(uint));
+                uint len = reader.ReadUInt32();
+                // 读内容
+                await reader.LoadAsync(len);
+                string msg = reader.ReadString(reader.UnconsumedBufferLength);
+
+                await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                {
+                    TextBlock tb = new TextBlock { Text = msg, FontSize = 20 };
+                    lbMsg.Children.Add(tb);
+                });
+                //runRecMsg.Text = msg;
+                // 释放
+                //reader.Dispose();
+
+            }
+            catch (Exception ex)
+            {
+                runRecMsg.Text = ex.Message;
+            }
+        }
+
+        private async void btConnectSocket_Click(object sender, RoutedEventArgs e)
+        {
+            if (socket != null)
+            {
+                await new MessageDialog("已经连接了Socket").ShowAsync();
+                return;
+            }
+            HostName hostName = null;
+            string message = "";
+            try
+            {
+                hostName = new HostName(runIp.Text);
+            }
+            catch (ArgumentException)
+            {
+                message = "主机名不可用";
+            }
+            if (message != "")
+            {
+                await new MessageDialog(message).ShowAsync();
+                return;
+            }
+            socket = new StreamSocket();
+            try
+            {
+                await socket.ConnectAsync(hostName, runPort.Text);
+                await new MessageDialog("连接成功").ShowAsync();      
+            }
+            catch (Exception exception)
+            {
+                if (SocketError.GetStatus(exception.HResult) == SocketErrorStatus.Unknown)
+                {
+                    throw;
+                }
+            }
+            await Task.Run(async () =>
+            {
+                //创建一个读取器 来读取服务端发送来的数据
+                try
+                {
+                    DataReader reader = new DataReader(socket.InputStream);
+                    //接受数据
+                    reader.UnicodeEncoding = UnicodeEncoding.Utf8; //注意
+                                                                   // 长度
+                    await reader.LoadAsync(sizeof(uint));
+                    uint len = reader.ReadUInt32();
+                    // 读内容
+                    await reader.LoadAsync(len);
+                    string msg = reader.ReadString(reader.UnconsumedBufferLength);
+
+                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        TextBlock tb = new TextBlock { Text = msg, FontSize = 20 };
+                        lbMsg.Children.Add(tb);
+                    });
+                }
+                catch (Exception exception)
+                {
+                    if (SocketError.GetStatus(exception.HResult) == SocketErrorStatus.Unknown)
+                    {
+                        throw;
+                    }
+                }
+            });
+        }
+
+        private async void btSendMsg_Click(object sender, RoutedEventArgs e)
+        {
+            if (socket == null)
+            {
+                await new MessageDialog("未连接Socket").ShowAsync();
+                return;
+            }
+
+
+            DataWriter writer = new DataWriter(socket.OutputStream);
+
+            string stringToSend = tbMsg.Text;
+            writer.WriteUInt32(writer.MeasureString(stringToSend));
+            writer.WriteString(stringToSend);
+
+            try
+            {
+                await writer.StoreAsync();
+                //getMsg();
+                //await new MessageDialog("发送成功").ShowAsync();
+            }
+            catch (Exception exception)
+            {
+                if (SocketError.GetStatus(exception.HResult) == SocketErrorStatus.Unknown)
+                {
+                    throw;
+                }
+            }
+        }
+
+        private async void getMsg()
+        {
+            DataReader reader = new DataReader(socket.InputStream);
+            reader.UnicodeEncoding = UnicodeEncoding.Utf8; //注意
+                                                           // 长度
+            await reader.LoadAsync(sizeof(uint));
+            uint len = reader.ReadUInt32();
+            // 读内容
+            await reader.LoadAsync(len);
+            string msg = reader.ReadString(reader.UnconsumedBufferLength);
+
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                TextBlock tb = new TextBlock { Text = msg, FontSize = 20 };
+                lbMsg.Children.Add(tb);
+            });
+        }
+
+        private async void btGetMsg_Click(object sender, RoutedEventArgs e)
+        {
+            DataReader reader = new DataReader(socket.InputStream);
+            reader.UnicodeEncoding = UnicodeEncoding.Utf8; //注意
+                                                           // 长度
+            await reader.LoadAsync(sizeof(uint));
+            uint len = reader.ReadUInt32();
+            // 读内容
+            await reader.LoadAsync(len);
+            string msg = reader.ReadString(reader.UnconsumedBufferLength);
+
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                TextBlock tb = new TextBlock { Text = msg, FontSize = 20 };
+                lbMsg.Children.Add(tb);
+            });
+        }
+    }
+}
---
+++ NEUSecretary/NEUSecretary/Turing.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Web.Http;
+using Windows.UI.Core;
+using static NEUSecretary.Models.TuringJson;
+using System.IO;
+using Windows.Data.Json;
+using Windows.UI.Popups;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
+
+namespace NEUSecretary
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class Turing : Page
+    {
+        public Turing()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void btnSendMsg_Click(object sender, RoutedEventArgs e)
+        {
+            ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+
+            var local = ApplicationData.Current.LocalFolder;
+            string postUrl = "http://www.tuling123.com/openapi/api";
+            if(localSettings.Values["stuId"]==null)
+            {
+                await new MessageDialog("由于您没有登陆不能调戏小秘书", "错误提示").ShowAsync();
+                return;
+            }
+            string id = localSettings.Values["stuId"].ToString();
+            string key = "4fd6c3d1d91d4463a6e5dc1af430fc62";
+            
+            await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                TextBlock tb = new TextBlock { Text = tbMsg.Text, FontSize = 20, HorizontalAlignment = HorizontalAlignment.Right };
+                lbMsg.Children.Add(tb);
+            });
+            
+
+            using (HttpClient client = new HttpClient())
+            {
+                var kvp = new List<KeyValuePair<string, string>>
+                {
+                    new KeyValuePair<string,string>("userid", id),
+                    new KeyValuePair<string,string>("info", tbMsg.Text),
+                    new KeyValuePair<string,string>("key", key),
+                };
+                var content = new HttpFormUrlEncodedContent(kvp);
+                HttpResponseMessage response = await client.PostAsync(new Uri(postUrl), content);
+                if (response.EnsureSuccessStatusCode().StatusCode.ToString().ToLower() == "ok")
+                {
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    string [] stringArr=responseBody.Split('"');
+                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+                    {
+                        TextBlock tb = new TextBlock { Text = stringArr[5], FontSize = 20 ,HorizontalAlignment=HorizontalAlignment.Left };
+                        lbMsg.Children.Add(tb);
+                    });
+                    
+                    // Jsonb JSONToObject(responseBody);
+                    //var json = JsonSerializer.Create();
+                    //Rootobject thinw = json.Deserialize<Rootobject>(new JsonTextReader(new StringReader(responseBody)));
+
+                    
+                }
+
+            }
+
+        }
+
+        private void ScrollText_ViewChanged(object sender, ScrollViewerViewChangedEventArgs e)
+        {
+
+        }
+    }
+}
+
